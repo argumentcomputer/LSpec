0244b0852837b354d2ab5e3355ff6f53723db229
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-06-21 23:23:31 -0300

    Problems:   * The original #lspec used `reduce`, which can introduce things like `Decidable.rec` and can't be evaluated   * Writing tests with booleans can be rather limited if the user wants the freedom to use `Prop` instead
    Solutions:
      * Use `#eval` under the hood, which takes care of evaluation for us
      * Create a new type class `TDecidable`, for which the user can provide instances that teach Lean how to resolve certain propositions
    
    Co-authored by Kyle Miller

--- LSpec/LSpec.lean
+++ LSpec/LSpec.lean
@@ -1,55 +1,85 @@
 import Lean
 
-abbrev Rel (α : Type _) := α → Bool 
-
-/-- 
-The basic structure used to encode specifications. 
-It represents a list of tests, with each test consisting of 
-a description `descr`, some value `a`, and a test `rel` for `a`.
+/--
+A variant of `Decidable` for tests. In the failing case, may give an explanatory
+message.
 -/
-inductive LSpec
-  | done
-  | more (descr : String) (a : α) (rel : Rel α) (next : LSpec)
-  deriving Inhabited
+class inductive TDecidable (p : Prop) where
+  | isFalse (h : ¬ p) (msg : Option Std.Format := none)
+  | isTrue  (h : p)
 
-/-- 
-`it` adds a single test to the testing suite.
-This is the same as the projection `LSpec.more`, but extracting it out 
-allows us to set `next` with the default value `done`.
--/
-def it (descr : String) (a : α) (rel : Rel α) (next : LSpec := LSpec.done) :=
-  LSpec.more descr a rel next
+instance (priority := low) (p : Prop) [d : Decidable p] : TDecidable p :=
+  match d with
+  | isFalse h => .isFalse h f!"Evaluated to false"
+  | isTrue  h => .isTrue  h
 
-/-- 
+/-- The basic structure used to encode a specification. -/
+structure LSpecTest where
+  descr : String
+  prop  : Prop
+  inst  : TDecidable prop
+
+/-- `test` is a single basic test. -/
+def test (descr : String) (p : Prop) [TDecidable p] : LSpecTest :=
+  ⟨descr, p, inferInstance⟩
+
+abbrev LSpecResult := String × Bool × Option Std.Format
+
+/--
 Prints the result of a test.
 -/
-def printMsg : String × Bool → String
-  | (d, true)  => s!"✓ it {d}"
-  | (d, false) => s!"× it {d}"
+def printLSpecResult : LSpecResult → String
+  | (d, b, msg) =>
+    let head := if b then s!"✓ {d}" else s!"× {d}"
+    match msg with
+    | some m => head ++ m.indentD.pretty
+    | none   => head
 
 /--
-Runs a set of `LSpec` tests and appends the results to 
-another list of results (given as input by the caller).
+The LSpec monad, records tests and whether they passed, along with an optional
+additional message.
 -/
-def runLSpec (results : List (String × Bool)) : LSpec → List (String × Bool)
-  | .more d a rel next => (d, rel a) :: (runLSpec results next)
-  | .done              => results.reverse
+abbrev LSpec := StateT (List LSpecResult) Id Unit
+
+def LSpec.addResult (d : String) (b : Bool) (m : Option Std.Format) : LSpec :=
+  StateT.modifyGet fun l => ((), (d, b, m) :: l)
+
+instance : Coe LSpecTest LSpec where coe
+  | ⟨d, _, .isTrue _⟩    => LSpec.addResult d true  none
+  | ⟨d, _, .isFalse _ m⟩ => LSpec.addResult d false m
 
 /--
-Generates a report for all the results in a `LSpec` test,
-returning `true` if all tests passed and `false` otherwise.
+Runs a set of `LSpec` tests and appends the results to another list of results
+(given as input by the caller).
 -/
-def compileLSpecResult (t : LSpec) : Bool × String :=
-  let res := runLSpec [] t
-  (res.foldl (init := true) fun acc (_, r) => acc && r,
-    "\n".intercalate $ res.map printMsg)
+def LSpec.run (tests : LSpec) : List (String × Bool × Option Lean.Format) :=
+  (StateT.run tests []).2.reverse
 
-/-- 
-Runs a `LSpec` test and pretty prints the results.
+/--
+Generates a report for all the results in a `LSpec` test, returning `true` if
+all tests passed and `false` otherwise.
 -/
-def lspec (s : String) (t : LSpec) (_ : List String) : IO UInt32 := do
-  IO.println s!"Testing that: {s}"
-  let (success?, msg) := compileLSpecResult t
-  if success?
-    then IO.println  msg; return 0
-    else IO.eprintln msg; return 1
+def LSpec.runAndCompile (t : LSpec) : Bool × String :=
+  let res := t.run
+  (res.foldl (init := true) fun acc (_, r, _) => acc && r,
+    "\n".intercalate <| res.map printLSpecResult)
+
+open Lean.Elab in
+def LSpec.toIOUnit (t : LSpec) : TermElabM Unit := do
+  let (success?, msg) := t.runAndCompile
+  if success? then
+    logInfo msg
+  else
+    throwError msg
+
+/-- todo -/
+macro "#lspec " term:term : command =>
+  `(#eval (LSpec.toIOUnit $term))
+
+/-- Runs a `LSpec` test suite and pretty prints the results. -/
+def lspec (t : LSpec) (_ : List String := []) : IO UInt32 := do
+  let (success?, msg) := t.runAndCompile
+  if success? then
+    IO.println  msg; return 0
+  else
+    IO.eprintln msg; return 1
