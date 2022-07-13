0222cb5a6543283dc3c40c7ccd401cb54609d3d0
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-13 18:21:34 -0300

    adding pipeline functions 'withOption...' and 'withExcept...' (#23)
    * adding pipeline functions 'withOption...' and 'withExcept...'
    
    * adding a new constructor to 'TDecidable' that represents a straightforward failure

--- LSpec/LSpec.lean
+++ LSpec/LSpec.lean
@@ -5,19 +5,28 @@ A variant of `Decidable` for tests. In the failing case, it may contain an
 explanatory message.
 -/
 class inductive TDecidable (p : Prop) where
-  | isFalse (h : ¬ p) (msg : Option Std.Format := none)
   | isTrue  (h : p)
+  | isFalse (h : ¬ p) (msg : Option Std.Format := none)
+  | isFailure (msg : Option Std.Format := none)
 
-/-- A default `TDecidable` instance with low priority -/
+/-- A default `TDecidable` instance with low priority. -/
 instance (priority := low) (p : Prop) [d : Decidable p] : TDecidable p :=
   match d with
   | isFalse h => .isFalse h f!"Evaluated to false"
   | isTrue  h => .isTrue  h
 
-/-- The datatype used to represent a sequence of tests -/
+/-- The datatype used to represent a sequence of tests. -/
 inductive TestSeq
   | more : String → (prop : Prop) → TDecidable prop → TestSeq → TestSeq
   | done
+
+/-- Appends two sequences of tests. -/
+def TestSeq.append : TestSeq → TestSeq → TestSeq
+  | done, t => t
+  | more d p i n, t' => more d p i $ n.append t'
+
+instance : Append TestSeq where
+  append := TestSeq.append
 
 /-- `test` allows the composition of tests. -/
 def test (descr : String) (p : Prop) [TDecidable p]
@@ -33,6 +42,7 @@ abbrev LSpec := StateT (List LSpecResult) Id Unit
 def TestSeq.toLSpec : TestSeq → LSpec
   | .more d _ (.isTrue _) n    => do set ((d, true, none) :: (← get)); n.toLSpec
   | .more d _ (.isFalse _ m) n => do set ((d, false, m) :: (← get));   n.toLSpec
+  | .more d _ (.isFailure m) n => do set ((d, false, m) :: (← get));   n.toLSpec
   | .done                      => pure ()
 
 instance : Coe TestSeq LSpec where
@@ -104,3 +114,36 @@ def lspecIO (t : LSpec) : IO UInt32 := do
   match lspec t with
   | .ok    msg => IO.println  msg; return 0
   | .error msg => IO.eprintln msg; return 1
+
+inductive ExpectationFailure (exp got : String) : Prop
+
+instance : TDecidable (ExpectationFailure exp got) :=
+  .isFailure s!"Expected '{exp}' but got '{got}'"
+
+/-- A test pipeline to run a function assuming that `opt` is `Option.some _` -/
+def withOptionSome (descr : String) (opt : Option α) (f : α → TestSeq) :
+    TestSeq :=
+  match opt with
+  | none   => test descr (ExpectationFailure "some _" "none")
+  | some a => test descr true $ f a
+
+/-- A test pipeline to run a function assuming that `opt` is `Option.none` -/
+def withOptionNone (descr : String) (opt : Option α) [ToString α]
+    (f : TestSeq) : TestSeq :=
+  match opt with
+  | none   => test descr true $ f
+  | some a => test descr (ExpectationFailure "none" s!"some {a}")
+
+/-- A test pipeline to run a function assuming that `exc` is `Except.ok _` -/
+def withExceptOk (descr : String) (exc : Except ε α) [ToString ε]
+    (f : α → TestSeq) : TestSeq :=
+  match exc with
+  | .error e => test descr (ExpectationFailure "ok _" s!"error {e}")
+  | .ok    a => test descr true $ f a
+
+/-- A test pipeline to run a function assuming that `exc` is `Except.error _` -/
+def withExceptError (descr : String) (exc : Except ε α) [ToString α]
+    (f : ε → TestSeq) : TestSeq :=
+  match exc with
+  | .error e => test descr true $ f e
+  | .ok    a => test descr (ExpectationFailure "error _" s!"ok {a}")
