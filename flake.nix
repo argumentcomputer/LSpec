030b6708348acd0521574bec93aa8671f5a8f92f
John C. Burnham <jcb@johnchandlerburnham.com>
2022-09-11 11:47:42 -0400

    update toolchain, add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A testing framework for Lean 4";
+
+  inputs.lean.url = github:leanprover/lean4;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, lean, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      leanPkgs = lean.packages.${system};
+      pkg = leanPkgs.buildLeanPackage {
+        name = "LSpec";  # must match the name of the top-level .lean file
+        src = ./.;
+      };
+    in {
+      packages = pkg // {
+        inherit (leanPkgs) lean;
+      };
+
+      defaultPackage = pkg.modRoot;
+    });
+}
