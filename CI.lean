ca8e2803f89f0c12bf9743ae7abbfb2ea6b0eeec
Arthur Paulino <arthurleonardo.ap@gmail.com>
2025-02-04 17:45:03 -0300

    update actions/checkout version

--- CI.lean
+++ CI.lean
@@ -16,7 +16,7 @@ jobs:
           curl -sSfL https://github.com/leanprover/elan/releases/download/v4.0.0/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
           ./elan-init -y --default-toolchain none
           echo \"$HOME/.elan/bin\" >> $GITHUB_PATH
-      - uses: actions/checkout@v2
+      - uses: actions/checkout@v4
       - name: run LSpec binary
         run: lake exe lspec
 "
