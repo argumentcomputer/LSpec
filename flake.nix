a6652a48b5c67b0d8dd3930fad6390a97d127e8d
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-03-13 21:24:22 -0400

    feat: Add Nix flake (#52)
    * feat: Add Nix flake
    
    * Simplify Nix build

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "LSpec Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    lean4-nix = {
+      url = "github:argumentcomputer/lean4-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    lean4-nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        system,
+        pkgs,
+        self',
+        config,
+        ...
+      }:
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(lean4-nix.readToolchainFile ./lean-toolchain)];
+        };
+
+        # Build the library with `nix build`
+        packages.default = ((lean4-nix.lake {inherit pkgs;}).mkPackage {
+            src = ./.;
+            roots = ["LSpec"];
+        }).modRoot;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs.lean; [lean lean-all];
+        };
+    };
+  };
+}
