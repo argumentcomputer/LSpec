3fb462de4fa8c3c9b1ef924356b8b358b29398cc
Leni Aniva <v@leni.sh>
2025-08-07 15:56:16 -0700

    chore: Update lean4-nix

--- flake.nix
+++ flake.nix
@@ -4,10 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    lean4-nix = {
-      url = "github:argumentcomputer/lean4-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    lean4-nix.url = "github:lenianiva/lean4-nix";
   };
 
   outputs = inputs @ {
