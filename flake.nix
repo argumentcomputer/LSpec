6643220e1f14e9abafcf25224f5efa42d3df67c7
Quinn Dougherty <quinn@for-all.dev>
2025-12-16 12:00:02 -0500

    Chore: update flake.nix for new lake2nix syntax

--- flake.nix
+++ flake.nix
@@ -26,8 +26,10 @@
         pkgs,
         self',
         config,
+        lib,
         ...
-      }: {
+      }: let lake2nix = pkgs.callPackage lean4-nix.lake {};
+        in {
         _module.args.pkgs = import nixpkgs {
           inherit system;
           overlays = [(lean4-nix.readToolchainFile ./lean-toolchain)];
@@ -35,10 +37,11 @@
 
         # Build the library with `nix build`
         packages.default =
-          ((lean4-nix.lake {inherit pkgs;}).mkPackage {
+          lake2nix.mkPackage {
+            name = "LSpec";
             src = ./.;
             roots = ["LSpec"];
-          }).modRoot;
+          };
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs.lean; [lean-all];
